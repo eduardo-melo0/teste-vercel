a18d0613ab4df8ba8692b7cea31dcec5aa634c91
eduardo-melo0 <eduardo.melo.pe@gmail.com>
2025-06-12 14:27:57 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}', // Adicione se usar uma pasta de componentes
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
