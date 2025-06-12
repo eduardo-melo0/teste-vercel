253621e1886ed7065936e88677c0080baaec9933
eduardo-melo0 <eduardo.melo.pe@gmail.com>
2025-06-12 12:16:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-javascript export default function HomePage() { return ( <div> <h1>A PÁGINA FUNCIONOU</h1> <p>O build do Next.js foi concluído com sucesso.</p> </div> ); } 
+export default function HomePage() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>A PÁGINA FUNCIONOU!</h1>
+      <p>O deploy do Next.js foi concluído com sucesso.</p>
+    </div>
+  );
+}
